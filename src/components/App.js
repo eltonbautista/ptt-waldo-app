f3ae3f4a303c941a6816f7c497db83a2fc800291
Elton Bautista <eltonbestaha@gmail.com>
2022-05-31 13:15:49 -0600

    Worked on a few states and effects

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import '../modules/styling-modules/App.css';
 import Navbar from './Navbar';
 import ImgContainer from './ImageContainer';
 import { useEffect, useState } from 'react';
-import styles from '../modules/styling-modules/ImageContainer.module.css'
 import { myWaldosArray } from '../firebase/firebase-config';
 
 
 function App() {
+
+  const initPointerState = 
+  {
+    top: 0,
+    left: 0,
+  }
+
+  const [pointerState, setPointerState] = useState(initPointerState);
+  // An array of my waldo objects, they will be passed down to the corresponding waldoButtonHandler
+  // Each button handler will turn the passed down object's isSelected property to true if the parameters are within the pointer's bounds
+  const [waldoState, setWaldoState] = useState(myWaldosArray);
 
   useEffect(() => {
     const myImage = document.querySelector('#universe113');
@@ -14,24 +24,25 @@ function App() {
     const waldoButtonContainer = document.querySelector('#waldo-button-container');
     const waldoButtons = document.querySelectorAll('button[data-waldo]');
 
-
     function myImageHandler(e) {
-      let foo = e.offsetX;
-      let bar = e.offsetY;
-      myPointer.style.left = foo - 25 + 'px';
-      myPointer.style.top = bar - 25 + 'px';
-      console.log(myWaldosArray);
-      // console.log(foo, bar);
-      
+
      if (waldoButtonContainer.style.visibility === 'visible') {
         myPointer.style.display = 'none';
         myPointer.style.visibility = 'hidden';
         waldoButtonContainer.style.visibility = 'hidden';
       } else {
+        let foo = e.offsetX - 25;
+        let bar = e.offsetY - 25;
         myPointer.style.display = 'block';
         waldoButtonContainer.style.visibility = 'visible';
         myPointer.style.visibility = 'visible';
+        myPointer.style.left = foo + 'px';
+        myPointer.style.top = bar + 'px';
+        pointerState.top = bar;
+        pointerState.left = foo;
+        setPointerState({...pointerState});
       }
+
   }
 
     function waldoButtonHandler(e) {
@@ -50,7 +61,7 @@ function App() {
         button.removeEventListener('click', waldoButtonHandler)
       })
     }
-  }, []);
+  }, [pointerState]);
 
   return (
     <div className="App" >
