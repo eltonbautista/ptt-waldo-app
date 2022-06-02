30e4be976fbeb1dbe8c6e1c438e4c83a14e1e328
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-01 18:04:29 -0700

    Update ImageContainer.js

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -3,16 +3,14 @@ import universe from '../assets/universe.png'
 import PointerTarget, { Marker } from "./PointerTarget";
 import styles from '../modules/styling-modules/ImageContainer.module.css';
 
-function ImgContainer(props) {
-  const { characters, clickCoords, buttonHandler, markerData } = props;
-
+function ImgContainer({ characters, buttonHandler, markerData, clicker }) {
   return (
-    <div className={styles['universe-container']} onClick={props.clicker} data-img-container id="img-container">
+    <div className={styles['universe-container']} onClick={clicker} data-img-container id="img-container">
       <img className={styles['universe-image']} src={universe} alt='universe113' id="universe113" />
       <PointerTarget characters={characters} buttonHandler={buttonHandler} />
       {markerData.length > 0 ? markerData.map((data) => {
         return (
-          <Marker markerName={data.markerName} left={data.left} top={data.top} />
+          <Marker key={data.myKey} markerName={data.markerName} left={data.left} top={data.top} />
         )
       }) : null}
     </div>
